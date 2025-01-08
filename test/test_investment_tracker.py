3b3842c99fe073ffd7abe023e2b9a7127f2e90f2
Vittorio Banfi <vittorio.banfi@gmail.com>
2025-01-08 09:21:44 +0100

    adjust test

--- test/test_investment_tracker.py
+++ test/test_investment_tracker.py
@@ -6,4 +6,5 @@ from investment_tracker import record_transaction
 class TestInvestment:
     def test_record_transaction(self):
         print("test_record_transaction")
+        record_transaction(100, "food", "Lunch at cafe")
         assert True
